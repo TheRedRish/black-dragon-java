374fea9cd7825addb948637c2879b4e7af088d87
TheRedRish <rune@roeddik.net>
2025-11-12 18:34:21 +0100

    Data Fetcher complete

--- src/main/java/dk/acto/blackdragon/service/impl/DataFetcherImpl.java
+++ src/main/java/dk/acto/blackdragon/service/impl/DataFetcherImpl.java
@@ -1,4 +1,25 @@
 package dk.acto.blackdragon.service.impl;
 
-public class DataFetcherImpl {
+import dk.acto.blackdragon.service.DataFetcher;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+// Note source: https://www.baeldung.com/java-9-http-client
+
+public class DataFetcherImpl implements DataFetcher {
+    @Override
+    public String fetchData(URL url) {
+        try {
+            HttpRequest request = HttpRequest.newBuilder().uri(url.toURI()).GET().build();
+            HttpResponse<String> response = HttpClient.newBuilder().build().send(request, HttpResponse.BodyHandlers.ofString());
+            return  response.body();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
